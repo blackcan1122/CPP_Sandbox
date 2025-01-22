525071d4ad404d83f402178f0b1f8ea10b786136
Marcel Schulz <marcel.s@live.de>
2025-01-22 15:44:16 +0100

    Button Lambda OnHover

--- src/private/TimeCalcMode.cpp
+++ src/private/TimeCalcMode.cpp
@@ -1,14 +1,42 @@
 #include "TimeCalcMode.h"
+#include "trex.h"
+#include "Logo.h"
+
 
 TimeCalcMode::TimeCalcMode(bool bIsDebug)
 {
+
+	image = LoadImageFromMemory(".jpg", trex_jpg, trex_jpg_len);     // Loaded in CPU memory (RAM)
+	imageLogo = LoadImageFromMemory(".png", Logo_png, Logo_png_len);     // Loaded in CPU memory (RAM)
+	texture = LoadTextureFromImage(image);          // Image converted to texture, GPU memory (VRAM)
+
+	SetWindowIcon(imageLogo);
+	UnloadImage(imageLogo);
+	UnloadImage(image);   // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
+
 
 }
 
 void TimeCalcMode::Update()
 {
+	ClearBackground(BLACK);
+
+	// Setup the back buffer for drawing (clear color and depth buffers)
+
+	DrawTexture(texture, GetScreenWidth() / 2 - texture.width / 2, GetScreenHeight() / 2 - texture.height / 2, DARKGRAY);
+
+	ActiveTimeCalc.UpdateCalculator();
+
+
+	DrawText("Kommen Zeit HH:MM :", GetScreenWidth() / 2.0f - 200, 200, 18, GREEN);
+	DrawText("Pausen Zeit HH:MM :", GetScreenWidth() / 2.0f - 200, 260, 18, GREEN);
+	DrawText("Korrektur Zeit HH:MM :", GetScreenWidth() / 2.0f - 223, 320, 18, GREEN);
+	DrawText("Restliche Qual:", GetScreenWidth() / 2.0f - 150, 380, 18, GREEN);
+
 }
 
 TimeCalcMode::~TimeCalcMode()
 {
+	UnloadTexture(texture);
+	std::cout << "Destructor is called" << std::endl;
 }
